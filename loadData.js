33441905fdcee7b0eab2ac7232715dceba2f4302
Dennis290699 <trujilloadrian29@gmail.com>
2024-08-01 21:09:32 -0500

    Update loadData.js

--- loadData.js
+++ loadData.js
@@ -29,22 +29,54 @@ async function insertData() {
     console.log('Conexión a la base de datos establecida.');
     console.log('Realizando carga de datos, espere por favor...');
 
+    const batch = [];
+
     // Saltar la fila de encabezado
     for (let i = 1; i < data.length; i++) {
       const [date, home_team, away_team, home_score, away_score, tournament, city, country, neutral] = data[i];
-
+    
       // Insertar en matches_by_country
-      await client.execute(insertMatchByCountry, [country, new Date(date), home_team, away_team, home_score, away_score, tournament, city, neutral === 'TRUE'], { prepare: true });
-
+      const matchByCountryQuery = {
+        query: insertMatchByCountry,
+        params: [country, new Date(date), home_team, away_team, home_score, away_score, tournament, city, neutral === 'TRUE']
+      };
+      batch.push(matchByCountryQuery);
+    
       // Insertar en matches_by_tournament
-      await client.execute(insertMatchByTournament, [tournament, new Date(date), home_team, away_team, home_score, away_score, country, city, neutral === 'TRUE'], { prepare: true });
+      const matchByTournamentQuery = {
+        query: insertMatchByTournament,
+        params: [tournament, new Date(date), home_team, away_team, home_score, away_score, country, city, neutral === 'TRUE']
+      };
+      batch.push(matchByTournamentQuery);
+    
+      // Ejecutar en lotes de 300 para evitar sobrecargar el sistema
+      if (batch.length === 300) {
+        try {
+          await client.batch(batch, { prepare: true });
+          //console.log(`Lote de 300 registros insertado con éxito.`);
+        } catch (batchError) {
+          console.error('Error al insertar el lote:', batchError);
+        }
+        batch.length = 0; // Vaciar el lote
+      }
     }
-
+    
+    // Ejecutar el lote restante
+    if (batch.length > 0) {
+      try {
+        await client.batch(batch, { prepare: true });
+        console.log(`Lote final insertado con éxito.`);
+      } catch (batchError) {
+        console.error('Error al insertar el lote final:', batchError);
+      }
+    }
+    
     const endTime = Date.now();
     console.log('Datos insertados exitosamente.');
     console.log(`Tiempo total empleado: ${endTime - startTime} ms`);
   } catch (error) {
     console.error('Error al insertar datos:', error);
+    console.error('Stack trace:', error.stack);
   } finally {
     await client.shutdown();
     console.log('Conexión a la base de datos cerrada.');
